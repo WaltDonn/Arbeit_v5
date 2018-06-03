1fc79bfe2dbbb51b20df0e44e8da285be77f6e49
Svayam Mishra <svayamm@andrew.cmu.edu>
2018-06-02 20:42:16 -0400

    base models generated
    Using schema from v4, generated new Rails 5 project and model files

---
+++ db/migrate/20180603003541_create_domains.rb
@@ -0,0 +1,10 @@
+class CreateDomains < ActiveRecord::Migration[5.1]
+  def change
+    create_table :domains do |t|
+      t.string :name
+      t.boolean :active
+
+      t.timestamps
+    end
+  end
+end
