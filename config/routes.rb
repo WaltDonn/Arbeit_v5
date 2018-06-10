2aabc976f46a5ce0e9ffe6ef47e5e51a129c2ae3
Svayam Mishra <svayamm@andrew.cmu.edu>
2018-06-09 21:00:49 -0400

    small changes ...
    ... updated routes file from v4; removed extraneous assignment controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,34 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Routes for main resources
+  resources :domains
+  resources :projects
+  resources :tasks
+  resources :assignments
+  resources :users
+  resources :sessions
+
+  # Authentication routes
+  get 'user/edit' => 'users#edit', as: :edit_current_user
+  get 'signup' => 'users#new', as: :signup
+  get 'logout' => 'sessions#destroy', as: :logout
+  get 'login' => 'sessions#new', as: :login
+
+  # Semi-static page routes
+  get 'home' => 'home#home', as: :home
+  get 'about' => 'home#about', as: :about
+  get 'contact' => 'home#contact', as: :contact
+  get 'privacy' => 'home#privacy', as: :privacy
+  get 'search' => 'home#search', as: :search
+  get 'cylon' => 'errors#cylon', as: :cylon
+
+  # Set the root url
+  root :to => 'home#home'
+
+  # Named routes
+  patch 'completed/:id' => 'tasks#complete', as: :complete
+  patch 'incomplete/:id' => 'tasks#incomplete', as: :incomplete
+  patch 'toggle_task/:id' => 'tasks#toggle', as: :toggle
+
+  # Last route in routes.rb that essentially handles routing errors
+  get '*a', to: 'errors#routing'
 end
