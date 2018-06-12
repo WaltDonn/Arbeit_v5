58f65f4b52199ef2982b2a5e321c36d45560f770
profh <profh@cmu.edu>
2018-06-12 13:44:06 -0400

    Added line to config/environments/ to have dev and test logs rotate daily

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Rotate logs daily to keep file sizes down
+  config.logger = Logger.new("log/#{Rails.env}.log", "daily")
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
