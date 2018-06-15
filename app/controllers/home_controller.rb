f513be909a6f721e6cbd7b4564eb6aebcd5967c7
Bernard Decelian <bdecelia@andrew.cmu.edu>
2018-06-15 05:45:19 -0400

    pagination for dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,11 @@ class HomeController < ApplicationController
       # get my projects
       @projects = current_user.projects.alphabetical.to_a
       project_ids = @projects.map(&:id)
-      
+
       # get my incomplete tasks
-      @incomplete_tasks = Task.by_priority.incomplete.map{|task| task if project_ids.include?(task.project_id)}
-      
+
+      incomplete_tasks = Task.by_priority.incomplete.map{|task| task if project_ids.include?(task.project_id)}.compact
+      @incomplete_tasks = Task.where(id: incomplete_tasks.map(&:id)).paginate(page: params[:page], per_page: 3)
       # get my completed tasks
       @completed_tasks = Task.by_name.completed.map {|task| task if project_ids.include?(task.project_id) }
     end
