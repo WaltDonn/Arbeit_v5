506b22de6acbd9674dca2bf8fa6a3121aff502fd
Bernard Decelian <bdecelia@andrew.cmu.edu>
2018-06-15 05:57:59 -0400

    colored tasks (dashboard)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,9 +6,9 @@ class HomeController < ApplicationController
       project_ids = @projects.map(&:id)
 
       # get my incomplete tasks
+      incomplete_tasks = Task.incomplete.map{|task| task if project_ids.include?(task.project_id)}.compact
+      @incomplete_tasks = Task.where(id: incomplete_tasks.map(&:id)).paginate(page: params[:page], per_page: 3).by_priority
 
-      incomplete_tasks = Task.by_priority.incomplete.map{|task| task if project_ids.include?(task.project_id)}.compact
-      @incomplete_tasks = Task.where(id: incomplete_tasks.map(&:id)).paginate(page: params[:page], per_page: 3)
       # get my completed tasks
       @completed_tasks = Task.by_name.completed.map {|task| task if project_ids.include?(task.project_id) }
     end
