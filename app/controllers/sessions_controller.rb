237b656c2df30dbcb67b566f6479376d4acb89e6
Bernard Decelian <bdecelia@andrew.cmu.edu>
2018-06-22 07:16:01 -0400

    unfiltered logs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < ApplicationController
   def new
   end
-  
+
   def create
     user = User.find_by_email(params[:email])
+    # logging user information for debugging purposes...
+    logger.info(User.find_by_email(params[:email]).as_json)
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
@@ -12,7 +14,7 @@ class SessionsController < ApplicationController
       render "new"
     end
   end
-  
+
   def destroy
     session[:user_id] = nil
     redirect_to root_url, notice: "Logged out!"
